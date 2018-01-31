643f6ff45d10d20c6db37a4dc654e302ebd4df42
Mattyx <grandemattyx@gmail.com>
2018-01-31 03:42:27 +0100

    script comment upgraded

--- js/form_validation.js
+++ js/form_validation.js
@@ -1,34 +1,49 @@
 function validate() {
+
     var comment = $("#TextArea").val();
     var sender = $("#Sender").val();
+    var ip;
 
-    if (comment == "") {
-        alert("il commento non deve essere vuoto");
-        return false;
-    }
+    fetch('https://freegeoip.net/json/', {headers: {}})
 
-    const json = {
-        comment: comment,
-        sender: sender
-    }
+        .then(function (data) {
 
-    console.log(json);
+            return data.json();
 
+        }).then(function (data) {
 
+        ip = data.ip;
 
-    $.ajax({
-        url: 'http://matteoomicini.drink-web.eu/api/new-comment',
-        type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify(json),
-        crossDomain:true,
-        success: function () {
-            alert('comment sent, take a look!');
-            $("#TextArea").val("");
-            $("#Sender").val("");
-        },
-        error: function () {
-            alert('error');
+    }).then(function () {
+
+        const json = {
+            comment: comment,
+            sender: sender,
+            address: ip
         }
+
+        console.log(json);
+
+        $.ajax({
+            url: 'http://matteoomicini.drink-web.eu/api/new-comment',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(json),
+            crossDomain: true,
+            success: function () {
+                alert('comment sent, take a look!');
+                $("#TextArea").val("");
+                $("#Sender").val("");
+            },
+            error: function () {
+                alert('error');
+            }
+        });
     });
+
 }
+
+
+
+
+
